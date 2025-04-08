de8bacbddc21645620929a6eefd337ebfa1bb934
Your Name <you@example.com>
2025-04-08 16:30:59 +0100

    'Initialisation_du_projet_de_gestion_de_stagiaire_backend_et_frontend'

---
+++ gestionstagebackend/src/main/java/gestionstagebackend/pac/gestionstagebackend/GestionstagebackendApplication.java
@@ -0,0 +1,13 @@
+package gestionstagebackend.pac.gestionstagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionstagebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionstagebackendApplication.class, args);
+	}
+
+}
---
+++ gestionstagebackend/src/test/java/gestionstagebackend/pac/gestionstagebackend/GestionstagebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gestionstagebackend.pac.gestionstagebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionstagebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
